e4e3d0d2b16fea45bb679152a4827f719adbbb55
LukasMut <muttenthaler@cbs.mpg.de>
2021-09-06 15:49:16 +0200

    added cuda and nvidia-smi parser

--- parse_cuda.sh
+++ parse_cuda.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-cuda_dir = "./cuda_infs"
-mkdir cuda_dir
+cuda_dir="./cuda_infs"
+mkdir -p ${cuda_dir};
+
 nvidia-smi > ${cuda_dir}/nvidia_smi.out
 python parse_cuda_version.py ${cuda_dir}/nvidia_smi.out > ${cuda_dir}/cuda_version.out
 
-input="./cuda_version.out"
+input="${cuda_dir}/cuda_version.out"
 while IFS= read -r line
 do
 	cuda_version=${line}
 done < "$input"
 
-printf "\n$cuda_version\n
+echo "CUDA Version on current node: $cuda_version"
 
 rm -r $cuda_dir
